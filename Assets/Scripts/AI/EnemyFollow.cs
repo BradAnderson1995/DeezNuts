e10d6eca680fb340cc8c4773a55531666f865900
silentcrow <chasken2@illinois.edu>
2015-09-05 17:55:39 -0500

    Enemies

---
+++ Assets/Scripts/AI/EnemyFollow.cs
@@ -0,0 +1,49 @@
+﻿using Assets.Scripts.Level;
+using UnityEngine;
+using Assets.Scripts.Player;
+
+namespace Assets.Scripts
+{
+    public class EnemyFollow : AbstractMovable
+    {
+        [SerializeField]   private int enemyHealth = 3;
+        [SerializeField]   private int damageDealt = 1;
+
+        private Transform playerPos;
+        private int moveX;
+        private int moveY;
+
+        void Start()
+        {
+            base.Start();
+            health = enemyHealth;
+            damage = damageDealt;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public override void Move()
+        {
+            moveX = 0;
+            moveY = 0;
+
+            playerPos = FindObjectOfType<PlayerController>().transform;
+            if (Mathf.Sqrt(Mathf.Pow((playerPos.position.x -transform.position.x),2) + Mathf.Pow((playerPos.position.y - transform.position.y),2)) < 80)
+            {
+                if (Mathf.Abs(playerPos.position.x - transform.position.x) > Mathf.Abs(playerPos.position.y - transform.position.y))
+                {
+                    moveX = playerPos.position.x > transform.position.x ? 1 : -1;
+                }
+                else
+                    moveY = playerPos.position.y > transform.position.y ? 1 : -1;
+                TryMove(new Vector2(transform.position.x + moveX*(LevelManager.UnitSize), transform.position.y + moveY*(LevelManager.UnitSize)), new Vector2(moveX,moveY));
+                print(moveX);
+                print(moveY);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/AI/EnemyRandom.cs
@@ -0,0 +1,87 @@
+﻿using Assets.Scripts.Level;
+using UnityEngine;
+
+namespace Assets.Scripts.AI
+{
+    public class Enemy2 : AbstractMovable
+    {
+        [SerializeField]   private int enemyHealth = 3;
+        [SerializeField]   private int damageDealt = 1;
+
+        public int moves = 2;
+
+        private System.Random rnd = new System.Random();
+        private int direction;
+        private int lastDir;
+        private bool trying;
+
+        void Start()
+        {
+            base.Start();
+            health = enemyHealth;
+            damage = damageDealt;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public override void Move()
+        {
+            direction = rnd.Next(0, 4);
+            while (moves > 0)
+            {
+                // 0: North 1: West 2: South    3: East
+                trying = true;
+                lastDir = 5;
+
+                while (trying)
+                {
+                    if (direction == lastDir)
+                    {
+                        while (direction == lastDir)
+                        {
+                            direction = rnd.Next(0,4);
+                        }
+                    }
+                    else if (direction == 0)
+                    {
+                        if (TryMove(new Vector2(transform.position.x, transform.position.y + LevelManager.UnitSize), new Vector2(0, 1f)))
+                            trying = false;
+                        direction = rnd.Next(1, 4);
+                    }
+                    else if (direction == 1)
+                    {
+                        if (TryMove(new Vector2(transform.position.x + LevelManager.UnitSize, transform.position.y), new Vector2(1f, 0)))
+                            trying = false;
+                        direction = rnd.Next(0, 4);
+                        while (direction == 1)
+                        {
+                            direction = rnd.Next(0, 4);
+                        }
+                    }
+                    else if (direction == 2)
+                    {
+                        if (TryMove(new Vector2(transform.position.x, transform.position.y - LevelManager.UnitSize), new Vector2(0, -1f)))
+                            trying = false;
+                        direction = rnd.Next(0, 4);
+                        while (direction == 2)
+                        {
+                            direction = rnd.Next(0, 4);
+                        }
+                    }
+                    else if (direction == 3)
+                    {
+                        if (TryMove(new Vector2(transform.position.x - LevelManager.UnitSize, transform.position.y), new Vector2(-1f, 0)))
+                            trying = false;
+                        direction = rnd.Next(0, 3);
+                    }
+                }
+                lastDir = direction;
+                moves--;
+            }
+        }
+    }
+}
