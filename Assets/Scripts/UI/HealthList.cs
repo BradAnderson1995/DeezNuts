b817bce641489d449a1656f62bd7b3cf970fe5f4
Brad Anderson <bradanderson1995@gmail.com>
2015-09-05 20:30:33 -0500

    Fixes tilesheet

--- Assets/Scripts/UI/HealthList.cs
+++ Assets/Scripts/UI/HealthList.cs
@@ -5,10 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class HealthList : MonoBehaviour {
-    public List<HealthContainer> containers;
+    public HealthContainer[] containers;
     // Use this for initialization
-	void Start () {
-        containers = GetComponentsInChildren<HealthContainer>().OrderByDescending(item => item.heartIndex).ToList();
+	void Start ()
+	{
+	    containers = GetComponentsInChildren<HealthContainer>().OrderByDescending(item => item.heartIndex).ToArray();
 	}
 	
 	// Update is called once per frame
