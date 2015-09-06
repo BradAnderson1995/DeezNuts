16646f2abe24ad890c51bc28cbcb49c353ed575c
Brad Anderson <bradanderson1995@gmail.com>
2015-09-05 20:23:00 -0500

    Adds dark tile

---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TitleController : MonoBehaviour {
+        [SerializeField] private string firstLevel;
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Submit"))
+            {
+                Application.LoadLevel(firstLevel);
+            }
+        }
+    }
+}
